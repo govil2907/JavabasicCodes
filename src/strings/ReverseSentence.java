1ebcc96464ab0cd80b7ecf3da57000fd4b0de57c
govil2907 <govilprof29@outlook.com>
2025-04-13 19:58:29 +0530

    Initial commit Pallindrome and ReverseSentence

---
+++ src/strings/ReverseSentence.java
@@ -0,0 +1,10 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReverseSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
