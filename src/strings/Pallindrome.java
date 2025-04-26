f857d512f98314c2c84888c44c600b83899eb61f
govil2907 <govilprof29@outlook.com>
2025-04-26 14:00:30 +0530

    commited on 26April'25

--- src/strings/Pallindrome.java
+++ src/strings/Pallindrome.java
@@ -2,7 +2,7 @@ package strings;
 
 import java.util.Scanner;
 
-public class Palindrome {
+public class Pallindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String");
